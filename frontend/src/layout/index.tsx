b40f5beaadcf94d6b4545ecf40cbb43694cf2a49
Eduardo Zielke <zielkeeduardo@gmail.com>
2022-09-16 00:34:15 -0300

    Adding new components

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,22 +1,60 @@
-import Header from './Header';
-import MyDrawer from './Drawer';
-import React from 'react';
-import { Box } from '@mui/system';
+import Header from "./Header";
+import MyDrawer from "./Drawer";
+import React from "react";
+import { Box } from "@mui/system";
+import { Container, Toolbar } from "@mui/material";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
+const drawerWidth = 240;
+
 const Layout: React.FC<Props> = ({ children }) => {
-    return (
-        <>
-            <Header />
-            <MyDrawer />
-            <Box>
-                {children}
-            </Box>
-        </>
-    )
-}
+  // return (
+  //   <>
+  //     <Header />
+  //     <Box display="flex">
+  //       <MyDrawer drawerWidth={drawerWidth} />
+  //       <Box
+  //         component="main"
+  //         sx={{
+  //           flexGrow: 1,
+  //           p: 3,
+  //           width: { sm: `calc(100% - ${drawerWidth}px)` },
+  //           backgroundColor: "red",
+  //         }}
+  //       >
+  //         {children}
+  //       </Box>
+  //     </Box>
+  //   </>
+  // );
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Header />
+      <MyDrawer drawerWidth={drawerWidth} />
+      <Box
+        component="main"
+        sx={{
+          // backgroundColor: (theme) =>
+          //   theme.palette.mode === 'light'
+          //     ? theme.palette.background.default
+          //     : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Toolbar />
+        <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+          {/* <Outlet /> */}
+          {children}
+        </Container>
+      </Box>
+    </Box>
+  )
+};
 
 export default Layout;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,14 @@
-import { CssBaseline } from '@mui/material'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import Header from './layout/Header'
+import { CssBaseline } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
